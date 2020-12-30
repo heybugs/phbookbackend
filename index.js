7e896d4eb3232474e064dbe2ce4592239ba5a47f
heybugs <jiajunhua2013@gmail.com>
2020-12-30 17:45:36 +0800

    change port config and add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan((tokens, req, res) => {
     return [
@@ -16,7 +19,8 @@ app.use(
     ].join(' ');
   })
 );
-const PORT = 3004;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
