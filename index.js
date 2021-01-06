9c456e0e0d143000d2b8091666339e9a331a4a43
heybugs <jiajunhua2013@gmail.com>
2021-01-06 14:45:42 +0800

    1遗留后台校验问题2.添加eslint校验

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const assert = require('assert')
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static('build'));
-require('dotenv').config();
-const Person = require('./models/person');
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+require('dotenv').config()
+const Person = require('./models/person')
 app.use(
   morgan((tokens, req, res) => {
     return [
@@ -19,92 +20,98 @@ app.use(
       tokens['response-time'](req, res),
       'ms',
       JSON.stringify(req.body),
-    ].join(' ');
+    ].join(' ')
   })
-);
-const PORT = process.env.PORT || 3001;
+)
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
-});
+  console.log(`server is running on http://localhost:${PORT}`)
+})
 
 // 默认页面
 app.get('/', (req, res) => {
-  res.send('<h1>Hello ！</h1>');
-});
+  res.send('<h1>Hello ！</h1>')
+})
 
 // 获取所有数据
 app.get('/api/persons', (req, res) => {
   Person.find({}).then((all) => {
-    res.json(all);
-  });
-});
+    res.json(all)
+  })
+})
 
 // 获取当前信息
 app.get('/info', (req, res) => {
-  let allCounter;
+  let allCounter
   Person.find({}).then((all) => {
-    allCounter = all.length;
-    let time = new Date();
+    allCounter = all.length
+    let time = new Date()
     let info = `
   <div>
   <h2>phonebook has info for ${allCounter} people</h2>
   <p>${time}</p>
   </div>
-  `;
-    res.send(info);
-  });
-});
+  `
+    res.send(info)
+  })
+})
 
 // 查询单独id信息
 app.get('/api/persons/:id', (req, res, next) => {
-  //5fed823456cfa4099b2b4c5d
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // 删除一条电话簿
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // 添加一条电话簿
 
 app.post('/api/persons', (req, res) => {
-  const body = req.body;
+  const body = req.body
 
   if (body.name === undefined && body.number === undefined) {
-    return res.status(400).json({ error: 'content missing' });
+    return res.status(400).json({ error: 'content missing' })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
     date: new Date(),
-  });
-  person.save().then((savePerson) => {
-    console.log(`saved success`);
-    res.json(savePerson);
-  });
-});
+  })
+  person
+    .save()
+    .then((savePerson) => {
+      res.json(savePerson)
+    })
+    .catch((error) => {
+      // TODO
+      assert.equal(error.errors['name'].message)
+      error = person.validateSync()
+      assert.equal(error.errors['number'].message)
+      res.send()
+    })
+})
 // 更新一条电话簿
 app.put('/api/persons/:id', (req, res) => {
   Person.update(
     { name: req.body.name },
     { $set: { number: req.body.number } }
-  ).then((result) => {
-    console.log(req.body.number);
-    res.status(200).end();
-  });
-});
+  ).then(() => {
+    res.status(200).end()
+  })
+})
--- models/person.js
+++ models/person.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI;
-console.log('connecting to ', url);
+const url = process.env.MONGODB_URI
+console.log('connecting to ', url)
 
 mongoose
   .connect(url, {
@@ -11,24 +12,46 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  // TODO
+  name: {
+    type: String,
+    required: [true, 'User name required'],
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return v.length >= 3
+      },
+      message: (props) => `${props.value} the name must more than 3 words`,
+    },
+  },
+  number: {
+    type: String,
+    required: [true, 'User phone number required'],
+    validate: {
+      validator: function (v) {
+        return v.split(',').join('').length >= 8
+      },
+      message: (props) =>
+        `${props.value} the phone number must more than 8 words!`,
+    },
+  },
   date: Date,
-});
+})
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
